0844cd8fb4c3e4e177770862dba19d7ef2213b0c
Richard Chang <76931794+rddmonkey@users.noreply.github.com>
2021-12-04 22:28:21 -0800

    Update 1108.py
    You can do it in one line with .replace, but since .replace isn't in even language probably not best practice.

--- 1108.py
+++ 1108.py
@@ -1,12 +1,5 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
 
-        ans = ""
-
-        for x in address:
-            if x == ".":
-                ans += "[.]"
-            else:
-                ans += x
-
-        return ans
+        address = address.replace(".","[.]")
+        return address
