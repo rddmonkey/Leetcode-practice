59658f2c5f510ee5d55aa8c762d10e7093254fab
Richard Chang <76931794+rddmonkey@users.noreply.github.com>
2021-12-12 22:14:55 -0800

    Update 1189.py
    This is the better python solution. Condenses it into one line.

--- 1189.py
+++ 1189.py
@@ -1,21 +1,4 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
 
-        bcount, acount, lcount,ocount,ncount = 0,0,0,0,0
-
-        for x in text:
-            if x == "b":
-                bcount += 1
-            if x == "a":
-                acount +=1
-            if x == "l":
-                lcount += 1
-            if x == "o":
-                ocount += 1
-            if x == "n":
-                ncount += 1
-
-        ocount = ocount//2
-        lcount = lcount//2
-
-        return min(bcount,acount,lcount,ocount,ncount)
+        return min(text.count("a"),text.count("b"),text.count("o")//2,text.count("l")//2,text.count("n"))
