2b774210d986cd81bcb514cfac0007e15855ddf5
Richard Chang <76931794+rddmonkey@users.noreply.github.com>
2021-12-13 12:35:52 -0800

    Create 1342.py

---
+++ 1342.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        ans = 0
+        
+        while num != 0:
+            if num % 2 == 0:
+                num /= 2
+                ans += 1
+            else:
+                num -= 1
+                ans += 1
+        return ans
